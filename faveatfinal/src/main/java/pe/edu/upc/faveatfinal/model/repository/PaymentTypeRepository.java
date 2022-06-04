c8021d2a526ce96162da56b96f3472922b139758
Brayan Rodriguez-Rojas <u201924448@upc.edu.pe>
2022-06-03 22:45:49 -0500

    Create Repository PaymentType and ReservationPayment

---
+++ faveatfinal/src/main/java/pe/edu/upc/faveatfinal/model/repository/PaymentTypeRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.faveatfinal.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pe.edu.upc.faveatfinal.model.entity.PaymentType;
+
+public interface PaymentTypeRepository extends JpaRepository<PaymentType, Integer>{
+
+}
