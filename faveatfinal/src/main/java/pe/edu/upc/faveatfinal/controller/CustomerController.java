f14ea2f07885f1ddbe98da4a3c5495d71654ef25
Nosmallero <diego_vasquez4@hotmail.com>
2022-06-04 18:11:49 -0500

    List-customer y controller

---
+++ faveatfinal/src/main/java/pe/edu/upc/faveatfinal/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.faveatfinal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+	@GetMapping
+	public String listCustomers() {
+		return "customers/list-customers";
+	}
+}
