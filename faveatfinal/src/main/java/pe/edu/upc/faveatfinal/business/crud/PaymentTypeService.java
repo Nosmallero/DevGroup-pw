a30512783400cc24ff456116203a25d36497df46
Brayan Rodriguez-Rojas <u201924448@upc.edu.pe>
2022-06-03 22:55:41 -0500

    Create Service PaymentType and ReservationPayment

---
+++ faveatfinal/src/main/java/pe/edu/upc/faveatfinal/business/crud/PaymentTypeService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.faveatfinal.business.crud;
+
+import pe.edu.upc.faveatfinal.model.entity.PaymentType;
+
+public interface PaymentTypeService extends CrudService<PaymentType, Integer>{
+
+}
