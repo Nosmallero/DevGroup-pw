c347e1907d2159d3714568464c9c7b1dc3433d0f
Nosmallero <diego_vasquez4@hotmail.com>
2022-06-26 20:25:45 -0500

    cambios en delivery

--- faveatfinal/src/main/java/pe/edu/upc/faveatfinal/controller/ExampleController.java
+++ faveatfinal/src/main/java/pe/edu/upc/faveatfinal/controller/ExampleController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ejemplo")
+@RequestMapping("/protocolo")
 public class ExampleController {
-	
+	@GetMapping	
+	public String soporte() {
+		return "soporte";
+	}
 }
